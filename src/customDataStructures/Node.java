eb3457816227534535c5351a6196eab51c725dc6
simon_hain <hainsimon2001@gmail.com>
2023-12-22 14:01:59 +0100

    Updated Winterwege Algorithm

--- src/customDataStructures/Node.java
+++ src/customDataStructures/Node.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Node<T>{
-    String name;
+    public String name;
     T value;
 
     //Each connected Node
--- src/problemImplementations/Winterwege.java
+++ src/problemImplementations/Winterwege.java
@@ -21,6 +21,12 @@ public class Winterwege {
         //Answer array, if there is 1 at answer[i][j], then there should be a heated path between nodei and nodej
         int[][] answer = new int[input.length][input.length];
 
+        for(int i = 0; i < answer.length; i++){
+            for(int j = 0; j < answer[0].length; j++){
+                answer[i][j] = 0;
+            }
+        }
+
         Node<Integer> startNode = nodes.get(0);
 
         //List of visited Nodes
@@ -32,6 +38,7 @@ public class Winterwege {
             //From each visited  parentNode, check connected Nodes for minimum Distance
             int smallestDistance = Integer.MAX_VALUE;
             Node<Integer> nextNode = null;
+            Node<Integer> parent = null;
             int i = -1;
             int j = -1;
             for(Node<Integer> parentNode : visitedNodes){
@@ -41,17 +48,24 @@ public class Winterwege {
                     if(!visitedNodes.contains(childNode) && distance <= smallestDistance){
                         smallestDistance = distance;
                         nextNode = childNode;
+                        parent = parentNode;
 
                     }
                 }
             }
             //Now we have to Node with the smallest distance, we add it
-            if(nextNode != null) {
-                distance.put(nextNode, smallestDistance);
-                visitedNodes.add(nextNode);
+            if(nextNode != null && parent != null) {
+                String childName = nextNode.name;
+                String parentName = parent.name;
+
+                int childNum = Integer.parseInt(childName.substring(4));
+                int parentNum = Integer.parseInt(parentName.substring(4));
+
+                answer[parentNum][childNum] = 1;
+
             }
         }
 
-
+        return answer;
     }
 }
