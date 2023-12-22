3f4d29b6f34cf30f05e752370d9c4196dbc52ac6
LouisGra <louis.grassmann@gmx.de>
2023-12-22 15:53:00 +0100

    Implementierung Klasse GraphScanner

--- src/customDataStructures/GraphScanner.java
+++ src/customDataStructures/GraphScanner.java
@@ -1,6 +1,37 @@
 package customDataStructures;
+import problemImplementations.Eiszeit.undirectedGraph;
+
+import java.util.*;
 
 public class GraphScanner
 {
+    public static undirectedGraph scanGraph() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Geben Sie die Anzahl der Knoten ein: ");
+        int numNodes = scanner.nextInt();
+
+        undirectedGraph graph = new undirectedGraph(numNodes);
+
+        for (int i = 0; i < numNodes; i++) {
+            System.out.print("Für Knoten " + i + ": Geben Sie die Anzahl der Kanten ein: ");
+
+
+            int numEdges = scanner.nextInt();
+            System.out.println();
+            for (int j = 0; j < numEdges; j++) {
+
+                System.out.print("	Geben Sie die Kante " + (j + 1) + " ein: ");
+                int neighbor = scanner.nextInt();
+
+                graph.addEdge(i, neighbor);
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+
+        return graph;
+    }
 
 }
