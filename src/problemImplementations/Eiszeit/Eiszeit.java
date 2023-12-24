20d64d27275cb259618e1809bca4ac6085cd030f
LouisGra <louis.grassmann@gmx.de>
2023-12-24 13:58:44 +0100

    Kommentare Klasse Eiszeit

--- src/problemImplementations/Eiszeit/Eiszeit.java
+++ src/problemImplementations/Eiszeit/Eiszeit.java
@@ -5,32 +5,36 @@ import java.util.*;
 
 public class Eiszeit {
     public static void main(String[] args) {
-        // Beispielgraph
+        //ein Referenzattribut der Klasse undirectredGraph wird erstellt, aber noch nicht initialisiert
         undirectedGraph graph;
+
+        //ein neues Objekt der Klasse GraphScanner wird erstellt
         GraphScanner scanner = new GraphScanner();
+
+        //Initialisierung des Referenzattribut graph: scanner.scanGraph() gibt einen Graphen zurück und initalisiert das Referenzatribut graph
         graph = scanner.scanGraph();
 
-
+        //Ausgabe des Graphen als Matrix und Liste
         System.out.println();
-        // Ausgabe als Adjazenzmatrix
-        graph.printAdjacencyMatrix();
+        graph.printAdjacencyMatrix();// Ausgabe des erstellten Graphen als Adjazenzmatrix
+        graph.printAdjacencyList();// Ausgabe des erstellten Grahphen als Adjazenzliste
 
-        // Ausgabe als Adjazenzliste
-        graph.printAdjacencyList();
-
+        //Hier wird eine Lite gespeichert, die Elemente des Typs Set<Integer> speichert. Diese enthält die Lösung, die erstellt wird beim Aufrufen der Methode .setCover(graph)
         List<Set<Integer>> solution = SetCover.setCover(graph);
 
+        //Ausgabe der Lösung
         System.out.println("Lösungs Sets: " + solution);
         System.out.println();
-        printNodesForEachSet(solution);
+        printNodesForEachSet(solution); //Aufruf der Methode printNodesForEachSet
 
     }
 
     public static void printNodesForEachSet(List<Set<Integer>> selectedSets) {
+        //Ausgabe der Anzahl der Knoten, um alle Kanten abzudecken
         System.out.println("Man benöitgt "+selectedSets.size()+ " Knoten, um alle Kanten abzudecken.");
         System.out.println();
         System.out.println("Hier sind die Knoten mit ihren jeweiligen Kanten, um alle Kanten abzudecken:");
-
+        // for schleife
         for (int i = 0; i < selectedSets.size(); i++) {
             System.out.println("Knoten "+(i + 1) + " mit folgenden Kanten: " + selectedSets.get(i));
         }
