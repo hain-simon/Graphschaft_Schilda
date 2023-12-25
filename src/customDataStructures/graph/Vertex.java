54189352e779789c5a758c4e356014a671fece30
simon_hain <hainsimon2001@gmail.com>
2023-12-25 10:24:02 +0100

    Removed unused imports

--- src/customDataStructures/graph/Vertex.java
+++ src/customDataStructures/graph/Vertex.java
@@ -1,9 +1,7 @@
 package customDataStructures.graph;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Vertex<T>{
     public String name;
