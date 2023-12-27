28ab8889fee1501e9722afaec8761aa21e7c3048
Joshii29 <116311347+Joshii29@users.noreply.github.com>
2023-12-28 00:34:13 +0100

    Integer erstellt

--- src/HistorischeFunde/HistorischeFunde.java
+++ src/HistorischeFunde/HistorischeFunde.java
@@ -1,5 +1,15 @@
 package HistorischeFunde;
 
 public class HistorischeFunde {
-    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public class Djikstra {
+        private final erste Bombe;
+        private final letzte Bombe;
+        private final List<Nod>
+    }
+
 }
