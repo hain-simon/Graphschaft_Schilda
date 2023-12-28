6534c4990525e5e75831c84f0a386cd09b344997
Joshua <joshua.stripp@gmail.com>
2023-12-29 00:51:00 +0100

    Main Methode aus HistorischeFunde gelöscht

--- src/problemImplementations/HistorischeFunde.java
+++ src/problemImplementations/HistorischeFunde.java
@@ -2,19 +2,12 @@ package problemImplementations;
 
 public class HistorischeFunde {
 
-    /*
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
-
 
     public class Djikstra {
         private final erste Bombe;
         private final letzte Bombe;
         private final List<Node>;
     }
-    */
 
 
 }
