dcd57d831b13e2c399c3e87df8f105f87d63df0e
simon_hain <hainsimon2001@gmail.com>
2023-12-28 16:14:10 +0100

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import customDataStructures.TopologicalSort;
+import problemImplementations.AktivitätsDiagramm;
+import customDataStructures.graph.TestInput;
+
 public class Main {
     public static void main(String[] args) {
 
         //Winterwege.solve(TestInput.getUndirectedGraph1());
         //SetCover.setCover(TestInput.getUndirectedGraph2());
 
-        //AktivitätsDiagramm.solve(TestInput.getDirectedGraph1());
+        AktivitätsDiagramm.solve(TestInput.getDirectedGraph1());
     }
 }
--- src/problemImplementations/HistorischeFunde.java
+++ src/problemImplementations/HistorischeFunde.java
@@ -2,14 +2,19 @@ package problemImplementations;
 
 public class HistorischeFunde {
 
+    /*
     public static void main(String[] args) {
         // TODO code application logic here
     }
 
+
+
     public class Djikstra {
         private final erste Bombe;
         private final letzte Bombe;
-        private final List<Nod>
+        private final List<Node>;
     }
+    */
+
 
 }
