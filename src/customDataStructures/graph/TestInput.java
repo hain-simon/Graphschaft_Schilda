d346da5350913c3e069f2b83efd786a26c0f0632
simon_hain <hainsimon2001@gmail.com>
2024-01-24 12:24:17 +0100

    Updated TestInput.java

--- src/customDataStructures/graph/TestInput.java
+++ src/customDataStructures/graph/TestInput.java
@@ -3,7 +3,7 @@ package customDataStructures.graph;
 public class TestInput {
 
     public static int[][] HistorischeFunde_TestGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 4, 2, 1, 0, 0},
                 {4, 0, 0, 0, 3, 0},
                 {2, 0, 0, 0, 0, 3},
@@ -11,7 +11,6 @@ public class TestInput {
                 {0, 3, 0, 2, 0, 3},
                 {0, 0, 3, 0, 3, 0}
         };
-        return graphArr;
     }
 
     public static int[][] Winterwege_TestGraph(){
@@ -19,7 +18,7 @@ public class TestInput {
     }
 
     public int[][] Aktivitaetsdiagramm_TestGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 0, 0, 0, 0, 0},
                 {1, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 1},
@@ -27,11 +26,10 @@ public class TestInput {
                 {0, 1, 0, 1, 0, 1},
                 {0, 0, 0, 0, 0, 0}
         };
-        return graphArr;
     }
 
     public int[][] Bierlieferung_TestGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 7, 0, 4, 10, 0},
                 {0, 0, 1, 0, 0, 3},
                 {0, 0, 0, 0, 0, 2},
@@ -39,11 +37,10 @@ public class TestInput {
                 {0, 0, 0, 12, 0, 4},
                 {0, 0, 0, 0, 0, 0}
         };
-        return graphArr;
     }
 
     public int[][] ProjektAufgaben_TestGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 3, 3, 3, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
                 {0, 0, 0, 0, 0, 1, 1, 1, 1, 0},
@@ -55,11 +52,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
-        return graphArr;
     }
 
     public int[][] Eiszeit_TestGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 1, 1, 0, 0, 1},
                 {1, 0, 1, 0, 1, 0},
                 {1, 1, 0, 1, 0, 0},
@@ -67,11 +63,10 @@ public class TestInput {
                 {0, 1, 0, 1, 0, 1},
                 {1, 0, 0, 0, 1, 0}
         };
-        return graphArr;
     }
 
     public int[][] Empfehlungssystem_TestGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 10, 4, 0, 0, 2},
                 {10, 0, 4, 0, 3, 0},
                 {4, 4, 0, 11, 0, 0},
@@ -79,13 +74,12 @@ public class TestInput {
                 {0, 3, 0, 2, 0, 6},
                 {2, 0, 0, 0, 6, 0}
         };
-        return graphArr;
     }
 
 
 
     public static int[][] getUndirectedGraph1(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 10, 0, 0, 0, 15},
                 {10, 0, 10, 0, 3, 0},
                 {0, 10, 0, 20, 0, 0},
@@ -93,11 +87,10 @@ public class TestInput {
                 {0, 3, 0, 7, 0, 4},
                 {15, 0, 0, 0, 4, 0}
         };
-        return graphArr;
     }
 
     public static int[][] getUndirectedGraph2(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 7, 0, 2, 0, 4}, //Vertex 0 has connection to Vertex 1 with weight 7, Vertex 3 with weight 2 ...
                 {7, 0, 1, 0, 0, 0}, //Vertex 1 has connection to Vertex 0 with weight 7 ....
                 {0, 1, 0, 5, 0, 0},
@@ -105,11 +98,10 @@ public class TestInput {
                 {0, 0, 0, 10, 0, 5},
                 {4, 0, 0, 1, 5, 0}
         };
-        return graphArr;
     }
 
     public static int[][] getFordFulkersonGraph(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 3, 3, 3, 0, 0, 3, 0, 0, 0}, //Vertex 0 has connection to Vertex 1 with weight 7, Vertex 3 with weight 2 ...
                 {0, 0, 0, 0, 1, 1, 0, 1, 1, 0}, //Vertex 1 has connection to Vertex 0 with weight 7 ....
                 {0, 0, 0, 0, 1, 1, 0, 1, 1, 0},
@@ -121,13 +113,12 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         };
-        return graphArr;
     }
 
 
 
     public static int[][] getDirectedGraph1(){
-        int[][] graphArr = new int[][]{
+        return new int[][]{
                 {0, 1, 0, 0, 0, 0},
                 {0, 0, 0, 1, 0, 0},
                 {0, 0, 0, 1, 0, 0},
@@ -135,7 +126,6 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 1},
                 {0, 0, 0, 0, 0, 0}
         };
-        return graphArr;
     }
 
 
@@ -148,7 +138,7 @@ public class TestInput {
 
 
     public static int[][] getUndirectedGraph4(){
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {1,0,1,1,1,0,1},
                 {0,1,1,1,0,0,1},
                 {1,1,0,1,0,1,0},
@@ -157,21 +147,19 @@ public class TestInput {
                 {0,0,1,0,1,1,0},
                 {1,1,1,1,0,0,1}
                 };
-        return kompetenzenMatrix;
     }
 
     public static int[][] getUndirectedGraph3(){
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {1,1,1,0},
                 {1,1,1,1},
                 {1,1,1,1},
                 {0,1,1,1}
         };
-        return kompetenzenMatrix;
 }
 
     public static int[][] getUndirectedGraph5() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
                 {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
@@ -183,11 +171,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 0}
         };
-        return kompetenzenMatrix;
     }
 
     public static int[][] getDirectedGraph4() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 2, 0, 0, 0, 0, 0},
                 {0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 4, 0, 0, 0},
@@ -199,11 +186,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 9},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
-        return kompetenzenMatrix;
     }
 
     public static int[][] getDirectedGraph3() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 2, 0, 0},
                 {0, 0, 3, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 4},
@@ -212,11 +198,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 6, 0}
         };
-        return kompetenzenMatrix;
     }
 
     public static int[][] directedGewichtet2() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 2, 0, 0, 0, 0, 0},
                 {1, 0, 3, 0, 0, 0, 0, 0, 0, 0},
                 {0, 3, 0, 0, 0, 0, 4, 0, 0, 0},
@@ -228,11 +213,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 7, 0, 0, 9},
                 {0, 0, 0, 0, 0, 0, 0, 8, 9, 0}
         };
-        return kompetenzenMatrix;
     }
 
     public static int[][] directedGewichtet1() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 2, 0, 0},
                 {0, 0, 3, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 4},
@@ -241,11 +225,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 6, 0}
         };
-        return kompetenzenMatrix;
     }
 
     public static int[][] UndirectedGewichtet1() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 2, 0, 0},
                 {1, 0, 3, 0, 0, 0, 0},
                 {0, 3, 0, 0, 0, 0, 4},
@@ -254,11 +237,10 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 4, 0, 0, 0, 0}
         };
-        return kompetenzenMatrix;
     }
 
     public static int[][] UndirectedGewichtet2() {
-        int[][] kompetenzenMatrix = {
+        return new int[][]{
                 {0, 1, 0, 0, 2, 0, 0, 0, 0, 0},
                 {1, 0, 3, 0, 0, 0, 0, 0, 0, 0},
                 {0, 3, 0, 0, 0, 0, 4, 0, 0, 0},
@@ -270,7 +252,6 @@ public class TestInput {
                 {0, 0, 0, 0, 0, 0, 7, 0, 0, 9},
                 {0, 0, 0, 0, 0, 0, 0, 8, 9, 0}
         };
-        return kompetenzenMatrix;
     }
 }
 
