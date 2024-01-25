88714bfbde4908d1002e16d34029477a624d5c0c
simon_hain <hainsimon2001@gmail.com>
2024-01-25 09:25:38 +0100

    Added Projektdokumentation.docx

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         //Winterwege.solve(TestInput.Winterwege_TestGraph());
-        //Eiszeit.solve(TestInput.Eiszeit_TestGraph());
+        Eiszeit.solve(TestInput.Eiszeit_TestGraph());
         //AktivitätsDiagramm.solve(TestInput.Aktivitaetsdiagramm_TestGraph());
         //ProjektAufgaben.projekteZuteilen(TestInput.ProjektAufgaben_TestGraph());
         //Empfehlungssystem.solve(TestInput.Empfehlungssystem_TestGraph(), 0, "Offen für Neues");
 
-        BierLieferung.solve(TestInput.Bierlieferung_TestGraph());
+        //BierLieferung.solve(TestInput.Bierlieferung_TestGraph());
 
         //HistorischeFunde.solve(TestInput.HistorischeFunde_TestGraph());
 
--- src/problemImplementations/Eiszeit.java
+++ src/problemImplementations/Eiszeit.java
@@ -6,14 +6,15 @@ import customDataStructures.SetCover;
 import customDataStructures.graph.Vertex;
 
 public class Eiszeit {
-    public static void solve(int[][] input){
+    public static CustomArrayList<Vertex<Integer>> solve(int[][] input){
         Graph<Integer> graph = new Graph<>(input);
         CustomArrayList<Vertex<Integer>> setCover = SetCover.solve(graph);
-        if(setCover == null) return;
+        if(setCover == null) return setCover;
 
         System.out.print("Vertices needed: ");
         for(Vertex<Integer> vertex : setCover){
             System.out.print("Knoten " +vertex.name + "; ");
         }
+        return setCover;
     }
 }
